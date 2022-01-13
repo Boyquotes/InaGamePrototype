fe73c503c632b0f690fd5e77715803fe96d2ed56
Emilio P <ea.pugaavalos@ugto.mx>
2022-01-13 16:07:15 -0600

    Added basic checkpoint system

---
+++ src/Interactive/Checkpoint/CheckPointArea.gd
@@ -0,0 +1,13 @@
+extends Area
+class_name Checkpoint
+
+signal activated_checkpoint()
+
+onready var audio_player = $Audio
+onready var respawn_point = $RespawnPoint
+onready var global_respawn_coord: Vector3 = respawn_point.global_transform.origin
+
+func _on_CheckPoint_body_entered(body: Node) -> void:
+	if body is Player:
+		audio_player.play()
+		emit_signal("activated_checkpoint", self)
---
+++ src/Interactive/Checkpoint/CheckPointSystem.gd
@@ -0,0 +1,28 @@
+extends Spatial
+
+export var death_area: NodePath
+export var player: NodePath
+
+var _active_checkpoint: Checkpoint
+var _player_node: Player
+var _death_area_node: Area
+
+func _ready() -> void:
+	_player_node = get_node(player)
+	_death_area_node = get_node(death_area)
+	
+	_death_area_node.connect("body_entered", self, "respawn_player")
+	
+	for child in get_children():
+		if child is Checkpoint:
+			child.connect("activated_checkpoint", self, "_on_activated_checkpoint")
+
+func respawn_player(body: Node) -> void:
+	if body is Player:
+		if _active_checkpoint != null:
+			_player_node.global_transform.origin = _active_checkpoint.global_respawn_coord
+		else:
+			_player_node.global_transform.origin = Vector3.ZERO
+
+func _on_activated_checkpoint(checkpoint: Checkpoint) -> void:
+	_active_checkpoint = checkpoint
