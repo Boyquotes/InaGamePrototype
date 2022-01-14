c819ac063bc728eab3ac1e22cb80afda4c52f3b5
Emilio P <ea.pugaavalos@ugto.mx>
2022-01-13 23:26:41 -0600

    Changed the popuptrigger to show a dialogic box, instead of the godot popup window

--- src/Interactive/PopupTrigger.gd
+++ src/Interactive/PopupTrigger.gd
@@ -2,22 +2,33 @@ extends Area
 
 var activated = false
 
-onready var accept_dialog = $AcceptDialog
+export(String, MULTILINE) var text = ""
+
 onready var sfx = $SFX
 
 func _on_PopupTrigger_body_entered(_body: Node) -> void:
-	if not activated and not get_tree().paused:
-		get_tree().paused = true
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		sfx.play()
-		accept_dialog.popup_centered()
-		activated = true
+	if activated or get_tree().paused:
+		return
+	
+	get_tree().paused = true
+	activated = true
+	sfx.play()
+	
+	var gdscript_dialog = Dialogic.start('')
+	gdscript_dialog.pause_mode = Node.PAUSE_MODE_PROCESS
+	
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	gdscript_dialog.connect("timeline_end", self, "_on_timeline_end")
+	gdscript_dialog.set_dialog_script( {
+		"events": [
+			{ "event_id":"dialogic_001", "text": text },
+			{ "event_id": "dialogic_022", "transition_duration": 0.2}
+		 ]
+	})
+	add_child(gdscript_dialog)
 
 
-func _on_AcceptDialog_confirmed() -> void:
-	get_tree().paused = false
+func _on_timeline_end(_timeline_name) -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-
-func _on_AcceptDialog_popup_hide() -> void:
-	_on_AcceptDialog_confirmed()
+	get_tree().paused = false
