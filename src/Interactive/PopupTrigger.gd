b4c8e4916d9e9ce8a6b79f90804f33e4d095c634
Emilio P <ea.pugaavalos@ugto.mx>
2022-01-14 16:25:42 -0600

    Change to project number version

--- src/Interactive/PopupTrigger.gd
+++ src/Interactive/PopupTrigger.gd
@@ -30,5 +30,5 @@ func _on_PopupTrigger_body_entered(_body: Node) -> void:
 
 
 func _on_timeline_end(_timeline_name) -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
--- src/Interactive/TextBox.gd
+++ src/Interactive/TextBox.gd
@@ -27,5 +27,5 @@ func show_dialog() -> void:
 
 
 func _on_timeline_end(_timeline_name) -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
