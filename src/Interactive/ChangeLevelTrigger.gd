96bf52595af3ebe8e6e64ca1681bf5571e14207a
Emilio P <ea.pugaavalos@ugto.mx>
2022-01-16 01:24:48 -0600

    ChangeLevelTrigger now grabs a random second in its sfx

--- src/Interactive/ChangeLevelTrigger.gd
+++ src/Interactive/ChangeLevelTrigger.gd
@@ -1,7 +1,14 @@
 extends Area
 
-export(String, FILE) var new_scene
+export(String, FILE, "*.tscn") var new_scene
+onready var sfx_player = $SFX
 
+func _ready() -> void:
+	var sfx_length = sfx_player.stream.get_length()
+	var rand_sec = rand_range(0.0, sfx_length)
+	print(rand_sec)
+	sfx_player.seek(rand_sec)
+	
 
 func _on_ChangeLevelTrigger_body_entered(body: Node) -> void:
 	if body is Player:
